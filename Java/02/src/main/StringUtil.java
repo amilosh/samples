e05e29a167ccc64d198a10b63d136e9cf3a8d57c
Alexander Milosh <milosh.alexander.by@gmail.com>
2024-12-08 01:46:20 +0100

    Update Java sample

---
+++ Java/02/src/main/StringUtil.java
@@ -0,0 +1,12 @@
+package main;
+
+public class StringUtil {
+
+    public String startsWith(String s) {
+        return String.valueOf(s.charAt(0));
+    }
+
+    public static int length(String s) {
+        return s.length();
+    }
+}
